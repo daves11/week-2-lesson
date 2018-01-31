512942e2eb6406c12f490bc4ea77f1d9c08612dd
daves11 <35769301+daves11@users.noreply.github.com>
2018-01-31 13:40:01 +0000

    Add files via upload

---
+++ js/string-variable.js
@@ -0,0 +1,18 @@
+//create variable to hold the name and the note text.
+var message;
+var message;
+
+//assign values to these variables.
+username='molly';
+message='see our upcoming range';
+
+//get the element with an id of name.
+var elname=document.getelnamebyid('name');
+//replace the content of this element.
+elname.textcontent=username;
+
+//get the element with an ud of note.
+var elnote=document.getelementnamebyid('note');
+//replace the content of this element.
+elnote.textcontent=message;
+ 
